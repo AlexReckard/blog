3978d19d0aaa1ad8853fc484390445f38aa3f61b
Alex Reckard <alexreckard@gmail.com>
2023-06-15 13:46:29 -0500

    legacy code folder

---
+++ legacy-code/js/lottie-scroll.js
@@ -0,0 +1,8 @@
+
+    var animation = bodymovin.loadAnimation({
+        container: document.getElementById('lottie-scroll'), // the DOM element that will contain the animation
+        renderer: 'svg',
+        loop: true,
+        autoplay: true,
+        path: 'https://assets6.lottiefiles.com/packages/lf20_P4RBQZ.json' // the path to the animation json
+    });
