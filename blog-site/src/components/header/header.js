61b566e6aac75c2047597640bfbb467650c3a6aa
Alex Reckard <alexreckard@gmail.com>
2023-06-15 14:41:47 -0500

    added dark mode feature

--- blog-site/src/components/header/header.js
+++ blog-site/src/components/header/header.js
@@ -1,8 +1,22 @@
-import React from 'react';
+import React, { useState } from 'react';
 import { Link } from 'gatsby';
 import './header.css';  
 
 const Header = () => {
+  const [darkMode, setDarkMode] = useState(false);
+
+  const handleToggle = () => {
+    setDarkMode(!darkMode);
+
+    if (!darkMode) {
+      document.body.classList.add('dark-mode');
+      document.documentElement.classList.add('dark-mode');
+    } else {
+      document.body.classList.remove('dark-mode');
+      document.documentElement.classList.remove('dark-mode');
+    }
+  };
+
   return (
     <header className="header-container">
       <div className="header-logo-container">
@@ -23,7 +37,13 @@ const Header = () => {
           <button aria-label="Close menu" className="close-menu"><i className="fas fa-times"></i></button>
           <button aria-label="hamburger menu" className="hamburger-menu"><i className="fas fa-bars"></i></button>
           <button aria-label="Search" className="header-search"><i className="fas fa-search"></i></button>
-          <button aria-label="Change theme" className="header-theme"><i className="fas fa-moon"></i></button>
+          <button 
+            aria-label="Change theme" 
+            className="header-theme"
+            onClick={handleToggle}
+          >
+            <i className={`fas ${darkMode ? 'fa-sun' : 'fa-moon'}`}></i>
+          </button>
         </div>
       </nav>
     </header>
