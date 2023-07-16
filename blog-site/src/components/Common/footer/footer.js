afd103b79da8335fa4a63b1e4c059d60eb785fa0
Alex Reckard <alexreckard@gmail.com>
2023-07-16 18:29:30 -0500

    newsletter email works with decoupled backend server

--- blog-site/src/components/Common/footer/footer.js
+++ blog-site/src/components/Common/footer/footer.js
@@ -1,22 +1,41 @@
-import React from 'react';
+import React, { useState } from 'react';
 import { Link } from 'gatsby';
 import './footer.css';  
 
 const Footer = () => {
+  const [message, setMessage] = useState(""); // to hold success or error message
+
+  const handleSubmit = async (event) => {
+    event.preventDefault();
+    const email = event.target.elements.email.value;
+    const response = await fetch('https://thawing-citadel-33891-7bc80812a736.herokuapp.com/subscribe', {
+      method: 'POST',
+      headers: { 'Content-Type': 'application/json' },
+      body: JSON.stringify({ email })
+    });
+    const data = await response.json();
+    if (data.success) {
+      setMessage("Subscribed successfully!");
+    } else {
+      setMessage("Subscription failed!");
+    }
+  }
+
   return (
-        <footer>
-        <div className="footer-content-container">
-            <div className="newsletter-container">
-                <div className="newsletter-content">
-                    <h2>SIGN UP TO OUR NEWSLETTER. COMPLETELY FREE, NO SRSLY.</h2>
-                    <form action="ACTUAL_SUBSCRIBE_URL" method="post">
-                        <input type="email" name="email" placeholder="EMAIL"/>
-                        <button type="submit">SUBSCRIBE</button>
-                    </form>
-                </div>
-                <div className="newsletter-disclaimer">
+    <footer>
+      <div className="footer-content-container">
+        <div className="newsletter-container">
+            <div className="newsletter-content">
+                <h2>SIGN UP TO OUR NEWSLETTER. COMPLETELY FREE, NO SRSLY.</h2>
+                <form onSubmit={handleSubmit}>
+                    <input type="email" name="email" placeholder="EMAIL"/>
+                    <button type="submit">SUBSCRIBE</button>
+                </form>
+                    {message && <p>{message}</p>} {/* Show message when it's not empty */}
+            </div>
+            <div className="newsletter-disclaimer">
                 <p>By subscribing, you agree to the <span><Link to="/privacyTerms">Terms of Use</Link></span> and <span><Link to="/privacyTerms">Privacy Policy</Link></span> & consent to receive periodic updates and promotional content from For The Reckard via Mailchimp. You can unsubscribe at any time. </p>
-                </div>
+            </div>
             </div>
             <div className="footer-links-container">
                 <ul className="footer-link-items">
